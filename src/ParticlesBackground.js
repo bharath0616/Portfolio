abe37a9b2c85bf124056d5d414ddb7fb315ab6a0
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2023-10-05 16:35:07 +0530

    pjs

--- src/ParticlesBackground.js
+++ src/ParticlesBackground.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Particles from 'react-tsparticles';
 import particlesConfig from './components/config/particles-config';
-
+import { loadFull } from "tsparticles";
 
 const ParticleComponent = () => {
-  return (
-    
-      <Particles id= "particles" options={particlesConfig} />
-    
-  );
-};
+  const particlesInit = async (main) => {
+		console.log(main);
+		await loadFull(main);
+	};
+	const particlesLoaded = (container) => {
+		console.log(container);
+	};
+	return (
+		<Particles
+			options={particlesConfig}
+			id="tsparticles"
+			init={particlesInit}
+			loaded={particlesLoaded}
+		/>
+	);
+}
+;
 
 export default ParticleComponent;
