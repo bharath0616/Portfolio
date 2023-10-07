530f3d8f2fa51883c0d1d9268b6b9fa8050ad404
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2023-10-07 23:08:50 +0530

    contact form emailjs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,22 +6,43 @@ import emailjs from '@emailjs/browser'
 
 const Contact = () => {
     const [letterClass, setLetterClass]=useState('text-animate')
+    const [name, setName] = useState(''); 
+    const [email, setEmail] = useState(''); 
+    const [subject, setSubject] = useState(''); 
+    const [message, setMessage] = useState(''); 
     const refForm = useRef();
     useEffect(() => {
          setTimeout(() => {
             setLetterClass('text-animate-hover')
-        },3980)
-        
+        },3980)      
     },[])
  
     const sendEmail = (e) => {
-        e.preventDefault()
-        emailjs.sendForm('gmail','template_3uoviwe',refForm.current ,'KWMHu6ndA21m79j4M')
-    .then(function(response) {
-       alert('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       alert('FAILED...', error);
-    });
+        e.preventDefault();
+        const serviceId='service_v744dz5'
+        const templateId='template_3uoviwe'
+        const publicKey='KWMHu6ndA21m79j4M'
+
+        const templateParams = {
+            from_name: name,
+            from_email:email,
+            to_name:'Bharath',
+            message:message
+        }
+        emailjs.send(serviceId,templateId,templateParams,publicKey)
+
+    .then(
+        (response) => {
+       alert('Message sent successfully!',response);
+       setName('');
+       setEmail('');
+       setSubject('');
+       setMessage('');
+    })
+    .catch((error) => {
+        alert("Failed to send the message, please try sending again.",error)
+    }
+    );
     }
 return(
     <div className='container1-wrapper'>
@@ -45,17 +66,17 @@ return(
     <form ref={refForm} onSubmit={sendEmail}>
         <ul>
             <li className='half'>
-                <input type="text" name="name" placeholder='Name'  required />
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Name'  required />
             </li>
             <li className='half'>
-                <input type="email" name="email" placeholder='Email' required />
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' required />
             </li>
             <l1>
-                <input placeholder='Subject' type='text' name="subject" required/>
+                <input placeholder='Subject' type='text' onChange={(e) => setSubject(e.target.value)} value={subject} required/>
             </l1>
 
             <l1>
-                <textarea placeholder='Enter your message'type='text' name='message' required></textarea>
+                <textarea placeholder='Enter your message'type='text' onChange={(e) => setMessage(e.target.value)} value={message} required></textarea>
             </l1>
             <l1>
                 <input 
