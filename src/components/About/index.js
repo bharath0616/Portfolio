aba4259e6a37651815ea0114d31c98f325eaadc9
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2023-10-22 08:55:18 +0530

    resume and about page initial

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,17 @@
 import './index.scss';
+import {
+    faNodeJs,
+    faCss3,
+    faGitAlt,
+    faHtml5,
+    faJsSquare,
+    faReact,
+  } from '@fortawesome/free-brands-svg-icons'
 import AnimatedLetters from '../AnimatedLetters';
 import Loader from 'react-loaders';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState,useEffect } from 'react';
+
 const About = () => {
     const [letterClass, setLetterClass]=useState('text-animate')
     useEffect(() => {
@@ -12,8 +22,9 @@ const About = () => {
     },[])
 return(
     <>
-    <div className='container contact-page'>
-    <div className='text-zone'>
+    <div className='top-container '>
+    <div className='container about-page about-container'>
+    <div className=' about-text-zone'>
         <h1>
             <AnimatedLetters
             letterClass={letterClass} 
@@ -21,12 +32,56 @@ return(
             idx={16}
              />
         </h1>
+        <p>
+            About me, I am <span>Bharath Mudduluru</span> from <span>Hyderabad,India.</span>
+            
+          </p>
+         
+          <p>
+          I am a pre-final year student pursuing BTech in Computer science and engineering with specialization in business systems.
+          </p>
+          <p>
+            Few of my hobbies include
+             Playing Games,Cubing,Travelling
+          </p>
+          <p>
+            You can look into my resume in next section for more information!
+          </p>
     </div>
+
+    <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faNodeJs} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div> 
         
+    </div>
+    
+    <div className='Resume'>
+    <a href="resume"className='flat-button'>Resume</a>
+    </div>
+    
     </div>
     <Loader type="pacman" />
     </>
+    
 )
-
 }
 export default About;
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,6 +2,10 @@ import './index.scss';
 import Loader from 'react-loaders';
 import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import React from 'react';
+import resume from '../../assets/Resume.pdf'
+
+
 const Resume = () => {
     const [letterClass, setLetterClass]=useState('text-animate')
     useEffect(() => {
@@ -12,15 +16,27 @@ const Resume = () => {
     },[])
 return(
     <>
-    <div className='container contact-page'>
-    <div className='text-zone'>
+    <div className='container2 Resume-page1'>
+    <div className='text-zone2'>
         <h1>
             <AnimatedLetters
             letterClass={letterClass} 
             strArray={['M','y','','R','e','s','u','m','e']}
-            idx={16}
+            idx={16}    
              />
         </h1>
+        
+    </div>
+    <div className='doc'>
+    <iframe
+        title="PDF Viewer"
+        src={resume}
+        width="100%"
+        height="800" ></iframe>
+    </div>
+
+    <div className='download'>
+    <a href="resume"className='flat-button'>Download</a>
     </div>
         
     </div>
