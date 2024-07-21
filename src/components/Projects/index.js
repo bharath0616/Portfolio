2daff1644110a40df8256087869efe8d457b756a
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2024-07-22 02:21:25 +0530

    fixed errors/alignments

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,44 +5,41 @@ import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 import Loader from 'react-loaders';
 
-
 const Project = (props) => {
-   
-return(
+  return (
     <>
-    <Card className="project-card-view">
-  <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-  <Card.Body>
-    <Card.Title style={{ textAlign: "center", fontSize: "2em", fontWeight: "bold" }}>
-      {props.title}
-    </Card.Title>
-    <Card.Text style={{ textAlign: "center",fontSize: "1.5em" }}>
-      {props.description}
-    </Card.Text>
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <Button variant="primary" href={props.ghLink} target="_blank">
-        <BsGithub /> &nbsp;
-        {props.isBlog ? "Blog" : "GitHub"}
-      </Button>
-      {!props.isBlog && props.demoLink && (
-        <Button
-          variant="primary"
-          href={props.demoLink}
-          target="_blank"
-          style={{ marginLeft: "10px" }}
-        >
-          <CgWebsite /> &nbsp;
-          {"Demo"}
-        </Button>
-      )}
-    </div>
-  </Card.Body>
-</Card>
+      <Card className="project-card-view">
+        <Card.Img variant="top" src={props.imgPath} alt="card-img" className="card-img-top" />
+        <Card.Body>
+          <Card.Title style={{ textAlign: "center", fontSize: "2em", fontWeight: "bold" }}>
+            {props.title}
+          </Card.Title>
+          <Card.Text style={{ textAlign: "center", fontSize: "1.5em" }}>
+            {props.description}
+          </Card.Text>
+          <div style={{ textAlign: "center", marginTop: "20px" }}>
+            <Button variant="primary" href={props.ghLink} target="_blank">
+              <BsGithub /> &nbsp;
+              {props.isBlog ? "Blog" : "GitHub"}
+            </Button>
+            {!props.isBlog && props.demoLink && (
+              <Button
+                variant="primary"
+                href={props.demoLink}
+                target="_blank"
+                style={{ marginLeft: "10px" }}
+              >
+                <CgWebsite /> &nbsp;
+                {"Demo"}
+              </Button>
+            )}
+          </div>
+        </Card.Body>
+      </Card>
 
-
-    <Loader type="pacman" />
+      <Loader type="pacman" />
     </>
-)
+  );
+};
 
-}
-export default Project
+export default Project;
